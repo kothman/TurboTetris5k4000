4000cbb0604de98b08443fd41930801cb7ef93cd
Colin Ledbetter <colinmledbetter@gmail.com>
2013-11-27 22:42:40 -0500

    Updated Grid

--- src/tetris/Grid.java
+++ src/tetris/Grid.java
@@ -1,4 +1,33 @@
-public class Grid{
-  ArrayList<Shape> shapes;
-  
+public class Grid {
+
+	ArrayList<Shape> shapes;
+
+	/**
+	*	Creates a grid to hold shapes/
+	*/
+	public Grid() {
+		shapes = new ArrayList<Shape>();
+	}
+
+	/**
+	*	Adds the specified shape to the grid.
+	*/
+	public void add(Shape s) {
+		shapes.add(s);
+	}
+
+	/**
+	*	Updates the grid a single tick.
+	*/
+	public void update() {
+		
+	}
+
+	/**
+	*	Returns the ArrayList of shapes stored in the grid.
+	*/
+	public ArrayList<Shape> getList() {
+		return shapes;
+	}
+
 }
