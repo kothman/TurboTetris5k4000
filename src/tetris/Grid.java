b0b18d82feeaa06fe3996b2c8285ed8e466f7aed
abkothma <abkothma@indiana.edu>
2013-11-27 23:01:25 -0500

    Made changes

--- src/tetris/Grid.java
+++ src/tetris/Grid.java
@@ -1,35 +1,37 @@
 import java.util.ArrayList;
+import javax.swing.JFrame;
 
-public class Grid {
+public class Grid extends JFrame{
 
-	ArrayList<Shape> shapes;
+ ArrayList<Shape> shapes;
 
-	/**
-	*	Creates a grid to hold shapes/
-	*/
-	public Grid() {
-		shapes = new ArrayList<Shape>();
-	}
 
-	/**
-	*	Adds the specified shape to the grid.
-	*/
-	public void add(Shape s) {
-		shapes.add(s);
-	}
+ /**
+ * Creates a grid to hold shapes/
+ */
+ public Grid() {
 
-	/**
-	*	Updates the grid a single tick.
-	*/
-	public void update() {
-		
-	}
+ }
 
-	/**
-	*	Returns the ArrayList of shapes stored in the grid.
-	*/
-	public ArrayList<Shape> getList() {
-		return shapes;
-	}
+ /**
+ * Adds the specified shape to the grid.
+ */
+ public void add(Shape s) {
+  shapes.add(s);
+ }
+
+ /**
+ * Updates the grid a single tick.
+ */
+ public void update() {
+  
+ }
+
+ /**
+ * Returns the ArrayList of shapes stored in the grid.
+ */
+ public ArrayList<Shape> getList() {
+  return shapes;
+ }
 
 }
