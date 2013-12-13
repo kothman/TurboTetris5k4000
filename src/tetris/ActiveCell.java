32c176136265ed5daa29d7c27f2da62c2b47c01b
abkothma <abkothma@indiana.edu>
2013-12-13 04:10:35 -0500

    Added ActiveCell.java

---
+++ src/tetris/ActiveCell.java
@@ -0,0 +1,23 @@
+package tetris;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class ActiveCell extends Cell {
+	int col, row;
+	ActiveCell(final int col, final int row,Cell.color c){
+		super();
+		this.col = col; this.row = row;
+		occupied=false;
+		this.currentColor = c;
+	}
+	void moveDown(){
+		row--;
+	}
+	public void paintComponent(Graphics g){
+		g.setColor(Color.WHITE);
+		g.drawRect(col*(Grid.CELL_PANEL_WIDTH/Grid.NUM_OF_COLS), row*(Grid.CELL_PANEL_HEIGHT/Grid.NUM_OF_ROWS), Grid.CELL_PANEL_WIDTH/Grid.NUM_OF_COLS, Grid.CELL_PANEL_HEIGHT/Grid.NUM_OF_ROWS);
+		g.setColor(this.getColor());
+		g.fillRect(col*(Grid.CELL_PANEL_WIDTH/Grid.NUM_OF_COLS) + 2, row*(Grid.CELL_PANEL_HEIGHT/Grid.NUM_OF_ROWS) + 2, Grid.CELL_PANEL_WIDTH/Grid.NUM_OF_COLS - 3, Grid.CELL_PANEL_HEIGHT/Grid.NUM_OF_ROWS - 3);
+	}
+}
